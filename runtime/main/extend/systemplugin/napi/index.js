18d8ecd6079c90902c94b48cf64b620234da156d
zhangxingxia <zhangxingxia1@huawei.com>
2022-01-26 16:28:36 +0800

    add storageStatistics and volumeManager api mock
    Signed-off-by: zhangxingxia <zhangxingxia1@huawei.com>

--- runtime/main/extend/systemplugin/napi/index.js
+++ runtime/main/extend/systemplugin/napi/index.js
@@ -56,6 +56,8 @@ import { mockConvertXml } from './convertxml'
 import { mockStatfs } from './statfs'
 import { mockFileManager } from "./filemanager"
 import { mockEnvironment } from "./environment"
+import { mockStorageStatistics } from "./storageStatistics"
+import { mockVolumeManager } from "./volumeManager"
 import { mockSettings } from './settings'
 import { mockAbilityAccessCtrl } from './abilityAccessCtrl'
 
@@ -148,6 +150,10 @@ export function mockRequireNapiFun() {
         return mockFileManager();
       case "environment":
         return mockEnvironment();
+      case "storagestatistics":
+        return mockStorageStatistics();
+      case "volumemanager":
+        return mockVolumeManager();
       case "settings":
         return mockSettings();
       case "abilityAccessCtrl":
