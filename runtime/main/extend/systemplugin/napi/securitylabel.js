4651ee07dd299db251d0147595101ad03b49bdcc
raoxian <raoxian@huawei.com>
2022-02-16 15:19:44 +0800

    update securitylabel.js
    Signed-off-by: raoxian <raoxian@huawei.com>

--- runtime/main/extend/systemplugin/napi/securitylabel.js
+++ runtime/main/extend/systemplugin/napi/securitylabel.js
@@ -22,10 +22,10 @@ export function mockSecurityLabel() {
         " Previewer may be different from that on a real device.")
       const len = args.length
       if (typeof args[len - 1] === 'function') {
-        args[len - 1].call(this, paramMock.businessErrorMock, paramMock.paramBooleanMock)
+        args[len - 1].call(this, paramMock.businessErrorMock)
       } else {
         return new Promise((resolve, reject) => {
-          resolve(paramMock.paramBooleanMock);
+          resolve();
         })
       }
     },
