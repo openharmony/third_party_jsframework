96e0f9b6dcbc3dfa2db62e4332ead1d12d9962c0
chennian <chennian1@huawei.com>
2022-05-30 09:58:54 +0800

    Signed-off-by:chennian<chennian1@huawei.com>
    Signed-off-by: chennian <chennian1@huawei.com>

--- runtime/main/extend/systemplugin/napi/ohos_privacyManager.js
+++ runtime/main/extend/systemplugin/napi/ohos_privacyManager.js
@@ -49,11 +49,11 @@ export function mockPrivacyManager() {
                 " Previewer may be different from that on a real device.")
             const len = args.length;
             if (len > 0 && typeof args[len - 1] === 'function') {
-                  args[len - 1].call(this, paramMock.businessErrorMock, paramMock.paramNumberMock)
+                  args[len - 1].call(this, paramMock.businessErrorMock, paramMock.paramNumberMock);
             } else {
                 return new Promise(resolve, reject => {
                     resolve(paramMock.paramNumberMock);
-                })
+                });
             }
         },
         getPermissionUsedRecords: function (...args) {
