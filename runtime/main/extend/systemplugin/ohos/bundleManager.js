af1ec890ee9ba5f8967c2f84e77889ecea81c770
耿文广 <gengwenguang@huawei.com>
2022-06-09 18:09:51 +0800

    新增api-mock
    Signed-off-by: 耿文广 <gengwenguang@huawei.com>

--- runtime/main/extend/systemplugin/ohos/bundleManager.js
+++ runtime/main/extend/systemplugin/ohos/bundleManager.js
@@ -888,39 +888,40 @@ export function mockBundleManager() {
 
   const ExtensionAbilityInfoMock = [
   {
-    bundleName: "[PC preview] unknown bundle name",
-    moduleName: "[PC preview] unknown module name",
-    name: "[PC preview] unknown name",
-    labelId: "[PC preview] unknown label id",
-    descriptionId: "[PC preview] unknown description id",
-    iconId: "[PC preview] unknown icon id",
-    isVisible: "[PC preview] unknown is visible",
-    extensionAbilityType: {
-      FORM: "[PC preview] unknown is FORM",
-      WORK_SCHEDULER: "[PC preview] unknown is WORK_SCHEDULER",
-      INPUT_METHOD: "[PC preview] unknown is INPUT_METHOD",
-      SERVICE: "[PC preview] unknown is SERVICE",
-      ACCESSIBILITY: "[PC preview] unknown is ACCESSIBILITY",
-      DATA_SHARE: "[PC preview] unknown is DATA_SHARE",
-      FILE_SHARE: "[PC preview] unknown is FILE_SHARE",
-      STATIC_SUBSCRIBER: "[PC preview] unknown is STATIC_SUBSCRIBER",
-      WALLPAPER: "[PC preview] unknown is WALLPAPER",
-      BACKUP: "[PC preview] unknown is BACKUP",
-      WINDOW: "[PC preview] unknown is WINDOW",
-      UNSPECIFIED: "[PC preview] unknown is UNSPECIFIED",
-    },
-    permissions: ["[PC preview] unknown permissions"],
-    applicationInfo: "[PC preview] unknown application info",
-    metadata: [{
+      bundleName: "[PC preview] unknown bundle name",
+      moduleName: "[PC preview] unknown module name",
       name: "[PC preview] unknown name",
-      value: "[PC preview] unknown value",
-      resource: "[PC preview] unknown resource",
-    }],
-    enabled: "[PC preview] unknown enabled",
-    readPermission: "[PC preview] unknown read permission",
-    writePermission: "[PC preview] unknown write permission",
-  }
-
+      labelId: "[PC preview] unknown label id",
+      descriptionId: "[PC preview] unknown description id",
+      iconId: "[PC preview] unknown icon id",
+      isVisible: "[PC preview] unknown is visible",
+      extensionAbilityType: {
+        FORM: "[PC preview] unknown is FORM",
+        WORK_SCHEDULER: "[PC preview] unknown is WORK_SCHEDULER",
+        INPUT_METHOD: "[PC preview] unknown is INPUT_METHOD",
+        SERVICE: "[PC preview] unknown is SERVICE",
+        ACCESSIBILITY: "[PC preview] unknown is ACCESSIBILITY",
+        DATA_SHARE: "[PC preview] unknown is DATA_SHARE",
+        FILE_SHARE: "[PC preview] unknown is FILE_SHARE",
+        STATIC_SUBSCRIBER: "[PC preview] unknown is STATIC_SUBSCRIBER",
+        WALLPAPER: "[PC preview] unknown is WALLPAPER",
+        BACKUP: "[PC preview] unknown is BACKUP",
+        WINDOW: "[PC preview] unknown is WINDOW",
+        UNSPECIFIED: "[PC preview] unknown is UNSPECIFIED",
+      },
+      permissions: ["[PC preview] unknown permissions"],
+      applicationInfo: "[PC preview] unknown application info",
+      metadata: [{
+        name: "[PC preview] unknown name",
+        value: "[PC preview] unknown value",
+        resource: "[PC preview] unknown resource",
+      }],
+      enabled: "[PC preview] unknown enabled",
+      readPermission: "[PC preview] unknown read permission",
+      writePermission: "[PC preview] unknown write permission",
+    }
+  ]
+  
   const ModuleUpdateFlagMock = {
     FLAG_MODULE_UPGRADE_CHECK: 0,
     FLAG_MODULE_UPGRADE_INSTALL: 1,
