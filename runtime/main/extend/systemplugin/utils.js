59a34b872cc0a11b462dd68277c916a0110070f7
li-yaoyao777 <liyaoyao7@huawei.com>
2022-06-15 10:56:12 +0800

    ohos_sensor.js
    Signed-off-by: li-yaoyao777 <liyaoyao7@huawei.com>

--- runtime/main/extend/systemplugin/utils.js
+++ runtime/main/extend/systemplugin/utils.js
@@ -24,14 +24,6 @@ export function getRandomArbitrary(min, max) {
   return Math.random().toFixed(6) * (max - min) + min;
 }
 
-// export const paramMock = {
-//   businessErrorMock: null,
-//   paramNumberMock: '[PC Preview] unknow number',
-//   paramStringMock: '[PC Preview] unknow string',
-//   paramBooleanMock: '[PC Preview] unknow boolean',
-//   paramObjectMock: '[PC Preview] unknow object',
-//   paramArrayMock: '[PC Preview] unknow Array'
-// };
 export const paramMock = {
   businessErrorMock: null,
   paramNumberMock: '[PC Preview] unknow number',
