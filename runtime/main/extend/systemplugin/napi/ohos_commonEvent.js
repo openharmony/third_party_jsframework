6a36ef10a1a2849bf90799e7520c9c49b33955c9
liuyanzhi <liuyanzhi3@huawei.com>
2022-07-06 17:16:32 +0800

    add notification mock
    Signed-off-by: liuyanzhi <liuyanzhi3@huawei.com>
    Change-Id: Ie2dc4787d78cb6de4c8c3a7374aa7645bd274ff9

--- runtime/main/extend/systemplugin/napi/ohos_commonEvent.js
+++ runtime/main/extend/systemplugin/napi/ohos_commonEvent.js
@@ -163,6 +163,18 @@ export function mockCommonEvent() {
         });
       }
     },
+    finishCommonEvent: function (...args) {
+        console.warn('CommonEventSubscriber.finishCommonEvent interface mocked in the Previewer. How this interface works on the' +
+          ' Previewer may be different from that on a real device.');
+        const len = args.length;
+        if (typeof args[len - 1] === 'function') {
+          args[len - 1].call(this, paramMock.businessErrorMock);
+        } else {
+          return new Promise((resolve, reject) => {
+            resolve();
+         });
+      }
+    },
   }
   const commonEvent = {
     publish: function (...args) {
