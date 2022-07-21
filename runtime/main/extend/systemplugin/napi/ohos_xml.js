799860c776477f026c258586c7e9a60f4f608ccb
lixingyang <lixingyang8@huawei.com>
2022-07-21 18:38:52 +0800

    Correct the inconsistency between Mock and d.ts of language compilation subsystem
    Signed-off-by: lixingyang <lixingyang8@huawei.com>
    https://gitee.com/openharmony/third_party_jsframework/issues/I5IA27

--- runtime/main/extend/systemplugin/napi/ohos_xml.js
+++ runtime/main/extend/systemplugin/napi/ohos_xml.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,67 +14,155 @@
  */
 
 import { paramMock } from "../utils"
-
 export function mockXml() {
-  const result = {
-    XmlSerializer: function (...args) {
-      console.warn("xml.XmlSerializer interface mocked in the Previewer. How this interface works on the Previewer" +
-        " may be different from that on a real device.")
-      return XmlSerializerMock;
-    },
-    XmlPullParser: function (...args) {
-      console.warn("xml.XmlPullParser interface mocked in the Previewer. How this interface works on the Previewer" +
-        " may be different from that on a real device.")
-      return XmlPullParserMock;
+  const XmlSerializerClass = class XmlSerializer {
+    constructor(...args) {
+      console.warn("XmlSerializer.constructor interface mocked in the Previewer. How this interface works on the Previewer" +
+        " may be different from that on a real device.");
+      this.setAttributes = function (...args) {
+        console.warn("XmlSerializer.setAttributes interface mocked in the Previewer. How this interface works on the Previewer" +
+        " may be different from that on a real device.");
+      };
+      this.addEmptyElement = function (...args) {
+        console.warn("XmlSerializer.addEmptyElement interface mocked in the Previewer. How this interface works on the Previewer" +
+        " may be different from that on a real device.");
+      };
+      this.setDeclaration = function (...args) {
+        console.warn("XmlSerializer.setDeclaration interface mocked in the Previewer. How this interface works on the Previewer" +
+        " may be different from that on a real device.");
+      };
+      this.startElement = function (...args) {
+        console.warn("XmlSerializer.startElement interface mocked in the Previewer. How this interface works on the Previewer" +
+        " may be different from that on a real device.");
+      };
+      this.endElement = function (...args) {
+        console.warn("XmlSerializer.endElement interface mocked in the Previewer. How this interface works on the Previewer" +
+        " may be different from that on a real device.");
+      };
+      this.setNamespace = function (...args) {
+        console.warn("XmlSerializer.setNamespace interface mocked in the Previewer. How this interface works on the Previewer" +
+        " may be different from that on a real device.");
+      };
+      this.setComment = function (...args) {
+        console.warn("XmlSerializer.setCommnet interface mocked in the Previewer. How this interface works on the Previewer" +
+        " may be different from that on a real device.");
+      };
+      this.setCData = function (...args) {
+        console.warn("XmlSerializer.setCData interface mocked in the Previewer. How this interface works on the Previewer" +
+        " may be different from that on a real device.");
+      };
+      this.setText = function (...args) {
+        console.warn("XmlSerializer.setText interface mocked in the Previewer. How this interface works on the Previewer" +
+        " may be different from that on a real device.");
+      };
+      this.setDocType = function (...args) {
+        console.warn("XmlSerializer.setDocType interface mocked in the Previewer. How this interface works on the Previewer" +
+        " may be different from that on a real device.");
+      }
     }
-  }
-  const XmlSerializerMock = {
-    setAttributes: function (...args) {
-      console.warn("XmlSerializer.setAttributes interface mocked in the Previewer. How this interface works on the Previewer" +
-        " may be different from that on a real device.")
-    },
-    addEmptyElement: function (...args) {
-      console.warn("XmlSerializer.addEmptyElement interface mocked in the Previewer. How this interface works on the Previewer" +
-        " may be different from that on a real device.")
-    },
-    setDeclaration: function (...args) {
-      console.warn("XmlSerializer.setDeclaration interface mocked in the Previewer. How this interface works on the Previewer" +
-        " may be different from that on a real device.")
-    },
-    startElement: function (...args) {
-      console.warn("XmlSerializer.startElement interface mocked in the Previewer. How this interface works on the Previewer" +
-        " may be different from that on a real device.")
-    },
-    endElement: function (...args) {
-      console.warn("XmlSerializer.endElement interface mocked in the Previewer. How this interface works on the Previewer" +
-        " may be different from that on a real device.")
-    },
-    setNamespace: function (...args) {
-      console.warn("XmlSerializer.setNamespace interface mocked in the Previewer. How this interface works on the Previewer" +
-        " may be different from that on a real device.")
-    },
-    setCommnet: function (...args) {
-      console.warn("XmlSerializer.setCommnet interface mocked in the Previewer. How this interface works on the Previewer" +
-        " may be different from that on a real device.")
-    },
-    setCData: function (...args) {
-      console.warn("XmlSerializer.setCData interface mocked in the Previewer. How this interface works on the Previewer" +
-        " may be different from that on a real device.")
-    },
-    setText: function (...args) {
-      console.warn("XmlSerializer.setText interface mocked in the Previewer. How this interface works on the Previewer" +
-        " may be different from that on a real device.")
-    },
-    setDocType: function (...args) {
-      console.warn("XmlSerializer.setDocType interface mocked in the Previewer. How this interface works on the Previewer" +
-        " may be different from that on a real device.")
+  };
+
+  const XmlPullParserClass = class XmlPullParser {
+    constructor(...args) {
+      console.warn("XmlPullParser.constructor interface mocked in the Previewer. How this interface works on the Previewer" +
+        " may be different from that on a real device.");
+      this.parse = function (...args) {
+        console.warn("XmlPullParser.parse interface mocked in the Previewer. How this interface works on the Previewer" +
+        " may be different from that on a real device.");
+      }
     }
-  }
-  const XmlPullParserMock = {
-    parse: function (...args) {
-      console.warn("XmlPullParser.parse interface mocked in the Previewer. How this interface works on the Previewer" +
-        " may be different from that on a real device.")
+  };
+
+  const xmlMock = {
+    XmlSerializer : XmlSerializerClass,
+    XmlPullParser : XmlPullParserClass,
+
+    EventType : {
+    START_DOCUMENT: 0,
+    END_DOCUMENT: 1,
+    START_TAG: 2,
+    END_TAG: 3,
+    TEXT: 4,
+    CDSECT: 5,
+    COMMENT: 6,
+    DOCDECL: 7,
+    INSTRUCTION: 8,
+    ENTITY_REFERENCE: 9,
+    WHITESPACE: 10
     }
-  }
-  return result;
+  };
+
+  const ParseInfoMock = {
+    getColumnNumber: function () {
+      console.warn("ParseInfo.getColumnNumber interface mocked in the Previewer. How this interface works on the Previewer" +
+      " may be different from that on a real device.")
+      return paramMock.paramNumberMock;
+    },
+    getDepth: function () {
+      console.warn("ParseInfo.getDepth interface mocked in the Previewer. How this interface works on the Previewer" +
+      " may be different from that on a real device.")
+      return paramMock.paramNumberMock;
+    },
+    getLineNumber: function () {
+      console.warn("ParseInfo.getLineNumber interface mocked in the Previewer. How this interface works on the Previewer" +
+      " may be different from that on a real device.")
+      return paramMock.paramNumberMock;
+    },
+    getName: function () {
+      console.warn("ParseInfo.getName interface mocked in the Previewer. How this interface works on the Previewer" +
+      " may be different from that on a real device.")
+      return paramMock.paramStringMock;
+    },
+    getNamespace: function () {
+      console.warn("ParseInfo.getNamespace interface mocked in the Previewer. How this interface works on the Previewer" +
+      " may be different from that on a real device.")
+      return paramMock.paramStringMock;
+    },
+    getPrefix: function () {
+      console.warn("ParseInfo.getPrefix interface mocked in the Previewer. How this interface works on the Previewer" +
+      " may be different from that on a real device.")
+      return paramMock.paramStringMock;
+    },
+    getText: function () {
+      console.warn("ParseInfo.getText interface mocked in the Previewer. How this interface works on the Previewer" +
+      " may be different from that on a real device.")
+      return paramMock.paramStringMock;
+    },
+    isEmptyElementTag: function () {
+      console.warn("ParseInfo.isEmptyElementTag interface mocked in the Previewer. How this interface works on the Previewer" +
+      " may be different from that on a real device.")
+      return paramMock.paramBooleanMock;
+    },
+    isWhitespace: function () {
+      console.warn("ParseInfo.isWhitespace interface mocked in the Previewer. How this interface works on the Previewer" +
+      " may be different from that on a real device.")
+      return paramMock.paramBooleanMock;
+    },
+    getAttributeCount: function () {
+      console.warn("ParseInfo.getAttributeCount interface mocked in the Previewer. How this interface works on the Previewer" +
+      " may be different from that on a real device.")
+      return paramMock.paramNumberMock;
+    }
+  };
+
+  const ParseOptionsMock = {
+    supportDoctype: '[PC preview] unknow supportDoctype',
+    ignoreNameSpace: '[PC preview] unknow ignoreNameSpace',
+    tagValueCallbackFunction: function(...args) {
+    console.warn("ParseOptions.tagValueCallbackFunction interface mocked in the Previewer. How this interface works on the Previewer" +
+      " may be different from that on a real device.")
+    return paramMock.paramNumberMock;
+    },
+    attributeValueCallbackFunction: function(...args) {
+    console.warn("ParseOptions.attributeValueCallbackFunction interface mocked in the Previewer. How this interface works on the Previewer" +
+      " may be different from that on a real device.")
+    return paramMock.paramNumberMock;
+    },
+    tokenValueCallbackFunction: function(...args) {
+    console.warn("ParseOptions.tokenValueCallbackFunction interface mocked in the Previewer. How this interface works on the Previewer" +
+      " may be different from that on a real device.")
+    return paramMock.paramNumberMock;
+    }
+  };
+  return xmlMock;
 }
