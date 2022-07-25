da0eb6bb97498f83a676e25e471db8e0ede28f00
weiyunxing <weiyunxing@huawei.com>
2022-07-25 14:55:07 +0800

    setting API mock
    Signed-off-by: weiyunxing <weiyunxing@huawei.com>

--- runtime/main/extend/systemplugin/napi/ohos_settings.js
+++ runtime/main/extend/systemplugin/napi/ohos_settings.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -107,7 +107,7 @@ export function mockSettings() {
     WIFI_STATUS: "[PC Preview] unknow WIFI_STATUS",
     WIFI_WATCHDOG_STATUS: "[PC Preview] unknow WIFI_WATCHDOG_STATUS",
   };
-  const settingsMock = {
+  const settings = {
     date,
     display,
     general,
