36e657894bc0e5c14d8c8f719edbbbc4633add6f
liuwei <liuwei595@huawei.com>
2022-08-05 15:29:17 +0800

    格式调整
    Signed-off-by: liuwei <liuwei595@huawei.com>

--- runtime/main/extend/systemplugin/napi/ohos_multimodalInput_pointer.js
+++ runtime/main/extend/systemplugin/napi/ohos_multimodalInput_pointer.js
@@ -32,14 +32,14 @@ export function mockPointer() {
     isPointerVisible: function (...args) {
       console.warn("multimodalInput.pointer.isPointerVisible interface mocked in the Previewer." +
         "How this interface works on the" + " Previewer may be different from that on a real device.");
-        const len = args.length
-        if (typeof args[len - 1] === 'function') {
-          args[len - 1].call(this, paramMock.businessErrorMock, paramMock.paramBooleanMock)
-        } else {
-          return new Promise((resolve) => {
-            resolve(paramMock.paramBooleanMock)
-          })
-        }
+      const len = args.length
+      if (typeof args[len - 1] === 'function') {
+        args[len - 1].call(this, paramMock.businessErrorMock, paramMock.paramBooleanMock)
+      } else {
+        return new Promise((resolve) => {
+          resolve(paramMock.paramBooleanMock)
+        })
+      }
     },
   };
   return pointer
