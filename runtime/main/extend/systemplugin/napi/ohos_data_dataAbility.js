41262a80859d34208468333272071e9faea269b0
ltdong <donglongtao@huawei.com>
2022-08-29 11:45:43 +0800

    preferences and rdb adapt for previewer on windows and mac
    Signed-off-by: ltdong <donglongtao@huawei.com>

--- runtime/main/extend/systemplugin/napi/ohos_data_dataAbility.js
+++ runtime/main/extend/systemplugin/napi/ohos_data_dataAbility.js
@@ -14,7 +14,163 @@
  */
 
 import { paramMock } from '../utils';
-import { RdbPredicatesClass } from './ohos_data_rdb'
+
+export const RdbPredicatesClass = class RdbPredicates {
+  constructor(...args) {
+    console.warn('data.rdb.RdbPredicates.constructor interface mocked in the Previewer. How this interface works on the' +
+      ' Previewer may be different from that on a real device.');
+    this.equalTo = function (...args) {
+      console.warn("RdbPredicates.equalTo interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.notEqualTo = function (...args) {
+      console.warn("RdbPredicates.notEqualTo interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.beginWrap = function (...args) {
+      console.warn("RdbPredicates.beginWrap interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.endWrap = function (...args) {
+      console.warn("RdbPredicates.endWrap interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.or = function (...args) {
+      console.warn("RdbPredicates.or interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.and = function (...args) {
+      console.warn("RdbPredicates.and interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.contains = function (...args) {
+      console.warn("RdbPredicates.contains interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.beginsWith = function (...args) {
+      console.warn("RdbPredicates.beginsWith interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.endsWith = function (...args) {
+      console.warn("RdbPredicates.endsWith interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.isNull = function (...args) {
+      console.warn("RdbPredicates.isNull interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.isNotNull = function (...args) {
+      console.warn("RdbPredicates.isNotNull interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.like = function (...args) {
+      console.warn("RdbPredicates.like interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.glob = function (...args) {
+      console.warn("RdbPredicates.glob interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.between = function (...args) {
+      console.warn("RdbPredicates.between interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.notBetween = function (...args) {
+      console.warn("RdbPredicates.notBetween interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.greaterThan = function (...args) {
+      console.warn("RdbPredicates.greaterThan interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.lessThan = function (...args) {
+      console.warn("RdbPredicates.lessThan interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.greaterThanOrEqualTo = function (...args) {
+      console.warn("RdbPredicates.greaterThanOrEqualTo interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.lessThanOrEqualTo = function (...args) {
+      console.warn("RdbPredicates.lessThanOrEqualTo interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.orderByAsc = function (...args) {
+      console.warn("RdbPredicates.orderByAsc interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.orderByDesc = function (...args) {
+      console.warn("RdbPredicates.orderByDesc interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.distinct = function (...args) {
+      console.warn("RdbPredicates.distinct interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.limitAs = function (...args) {
+      console.warn("RdbPredicates.limitAs interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.offsetAs = function (...args) {
+      console.warn("RdbPredicates.offsetAs interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.groupBy = function (...args) {
+      console.warn("RdbPredicates.groupBy interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.indexedBy = function (...args) {
+      console.warn("RdbPredicates.indexedBy interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.in = function (...args) {
+      console.warn("RdbPredicates.in interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.notIn = function (...args) {
+      console.warn("RdbPredicates.notIn interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.inDevices = function (...args) {
+      console.warn("RdbPredicates.inDevices interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+    this.inAllDevices = function (...args) {
+      console.warn("RdbPredicates.inAllDevices interface mocked in the Previewer. How this interface works on the" +
+        " Previewer may be different from that on a real device.")
+      return new RdbPredicatesClass();
+    };
+  }
+};
 
 export function mockDataAbility() {
   const DataAbilityPredicatesClass = class DataAbilityPredicates {
