149476d271bacb3a14f284d8119d65852ddee4c9
LVB8189 <lvbai1@huawei.com>
2022-09-30 17:02:06 +0800

    Signed-off-by: LVB8189 <lvbai1@huawei.com>
     Changes to be committed:

--- runtime/main/extend/systemplugin/napi/ohos_screenLock.js
+++ runtime/main/extend/systemplugin/napi/ohos_screenLock.js
@@ -68,10 +68,10 @@ export function mockScreenLock() {
         " Previewer may be different from that on a real device.")
       const len = args.length
       if (typeof args[len - 1] === 'function') {
-        args[len - 1].call(this, paramMock.businessErrorMock)
+        args[len - 1].call(this, paramMock.businessErrorMock, paramMock.paramBooleanMock)
       } else {
         return new Promise((resolve) => {
-          resolve();
+          resolve(paramMock.paramBooleanMock);
         })
       }
     },
