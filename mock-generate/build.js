6728595068176cc32180764a1d7ef3c513072dd8
buzhifeng <buzhifeng2@huawei.com>
2022-10-17 17:57:44 +0800

    mock napi index 文件导入重名适配
    Signed-off-by: buzhifeng <buzhifeng2@huawei.com>

--- mock-generate/build.js
+++ mock-generate/build.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { spawn } = require('child_process');
+const { spawnSync } = require('child_process');
 const path = require('path');
 const os = require('os');
 function compileMock() {
@@ -11,36 +11,11 @@ function compileMock() {
     nodeDir = './node-v12.18.4-darwin-x64/bin/node';
   }
   
-  const bat = spawn(`${path.join(__dirname, '..', nodeDir)} ${path.join(__dirname, '..','./node_modules/typescript/bin/tsc')} && 
+  const bat = spawnSync(`${path.join(__dirname, '..', nodeDir)} ${path.join(__dirname, '..','./node_modules/typescript/bin/tsc')} && 
   ${path.join(__dirname, '..', nodeDir)} ${path.join(__dirname, 'dist')}/main.js && 
   ${path.join(__dirname, '..', nodeDir)} ${path.join(__dirname, '..', './node_modules/eslint/bin/eslint.js')} -c .eslintrc --fix ${mockJsPath}/**/*.js`, {
     cwd: __dirname,
     shell: true
-  });
-  shellOutput(bat);
-}
-
-function shellOutput(bat) {
-  bat.stdout.on('data', (data) => {
-    const lines = `${data}`.split(/\r{0,1}\n/);
-    if (lines) {
-      lines.forEach(line => {
-        if (line.length > 0) {
-          console.log(line.toString());
-        }
-      });
-    }
-  });
-
-  bat.stderr.on('data', (data) => {
-    const lines = `${data}`.split(/\r{0,1}\n/);
-    if (lines) {
-      lines.forEach(line => {
-        if (line.length > 0) {
-          console.log(line.toString());
-        }
-      });
-    }
   });
 }
 
